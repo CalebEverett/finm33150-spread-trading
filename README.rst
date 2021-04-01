54c54840491fee2e44539202f017b8c539915204
calebeverett <caleb@calebeverett.io>
2021-04-01 16:06:43 -0700

    completed data wrangling

--- README.rst
+++ README.rst
@@ -1,36 +1,24 @@
 ===============================
-Futures Spreads
+FINM33150 - Futures Spreads
 ===============================
 
-|build| |maintain| |coverage|
+Homework assignment for FINM33150
 
-.. |build| image:: https://github.com/CalebEverett/finm33150-futures-spreads/workflows/build%20status/badge.svg
-    :target: https://github.com/CalebEverett/finm33150-futures-spreads/actions?query=workflow%3A%22build+status%22
-    :alt: Build Status
-
-.. |maintain| image:: https://api.codeclimate.com/v1/badges/ec91b5c89c085f918cf2/maintainability
-   :target: https://codeclimate.com/repos/ec91b5c89c085f918cf2/maintainability
-   :alt: Maintainability
-
-.. |coverage| image:: https://api.codeclimate.com/v1/badges/ec91b5c89c085f918cf2/test_coverage
-   :target: https://codeclimate.com/repos/ec91b5c89c085f918cf2/test_coverage
-   :alt: Test Coverage
-
-.. contents:: Table of Contents
-
-First Problem
+Data
 -------------
 
-Describe solution implementation.
+Obtain data from Quandl to calculate second month futures prices.
 
 * Super cool feature one.
 * Super cool feature two.
 
-Second Problem
+Analysis
 --------------
 
 Describe solution implementation.
 
 * Super cool feature one.
 * Super cool feature two.
+
+
 
--- submit.py
+++ submit.py
@@ -1,5 +1,6 @@
 import os
 
+from canvasapi import Canvas
 from canvasapi.quiz import QuizSubmissionQuestion
 from typing import List, Dict
 
@@ -27,9 +28,34 @@ if __name__ == "__main__":
     token = os.getenv("CANVAS_TOKEN")
 
     # sm = SubmissionManager(
-    #     pset=1, min_quiz_score=10, canvas_url=url, canvas_token=token
+    #     assignment_name="Assignment Futures Spreads",
+    #     course_name="FINM 33150 1, 2 (Spring 2021) Regression Analysis and Quantitative Trading Strategies",
+    #     canvas_url=url,
+    #     canvas_token=token,
     # )
 
     # sm.get_canvas_objects()
+    # print(sm.course)
     # sm.atomic_quiz_submit(answers_fn, verbose=True)
     # sm.assignment_submit(verbose=True)
+
+    course_id = 33395
+    canvas = Canvas(url, token)
+    course = canvas.get_course(course_id)
+
+    # for f in course.get_files():
+    #     if f.folder_id == 842539:
+    #         file = course.get_file(f.id)
+    #         file.download(file.filename)
+
+    # file = course.get_file(5394394)
+    # file.download(file.filename)
+
+    folder = course.get_folder(842539)
+
+    for f in folder.get_files():
+        try:
+            f.download(f"notebooks/{f.filename}")
+            print("downloaded", f.filename)
+        except:
+            print("failed", f.filename, f.url)
